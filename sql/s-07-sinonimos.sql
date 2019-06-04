bb30fd8aa865dc7ccf2bad49ec869679ddbc043f
Alm1337 <andreslopezmartinez@hotmail.com>
2019-06-03 21:47:39 -0500

    Modifique sinonimos

--- sql/s-07-sinonimos.sql
+++ sql/s-07-sinonimos.sql
@@ -4,28 +4,32 @@
 
 connect lm_proy_admin/admin
 
-
-create or replace public synonym info_status_viaje 
+create or replace public synonym status_viaje 
 	for status_viaje;
-create or replace public synonym info_modelo 
+create or replace public synonym modelo 
 	for modelo;
-create or replace public synonym info_marca
+create or replace public synonym marca
 	for marca;
-
 
 grant select on lm_proy_admin.modelo to lm_proy_invitado;
 grant select on lm_proy_admin.marca to lm_proy_invitado;
 grant select on lm_proy_admin.status_viaje to lm_proy_invitado;
+grant select on lm_proy_admin.conductor to lm_proy_invitado;
+grant select on lm_proy_admin.auto to lm_proy_invitado;
+grant select on lm_proy_admin.ubicacion to lm_proy_invitado;
 
+connect sys/system as sysdba
+
+grant create synonym to lm_proy_invitado;
 
 connect lm_proy_invitado/invitado
 
-create or replace synonym cliente_inv
-	for lm_proy_admin.modelo;
-create or replace synonym conductor_inv
-	for lm_proy_admin.marca;
-create or replace synonym administrador_inv
-	for lm_proy_admin.status_viaje;
+create or replace synonym conductor
+	for lm_proy_admin.conductor;
+create or replace synonym auto
+	for lm_proy_admin.auto;
+create or replace synonym ubicacion
+	for lm_proy_admin.ubicacion;
 
 connect lm_proy_admin/admin
 
@@ -39,7 +43,7 @@ begin
 	for r in cur_tablas loop 
 		execute immediate 'create or replace synonym XX_'
 			|| r.table_name
-			|| ' for lm_proy_admin.'
+			|| ' for '
 			|| r.table_name;
 	end loop; 
 end;
