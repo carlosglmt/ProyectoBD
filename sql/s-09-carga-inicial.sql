75778306279eea205efa0df3a5c922c6a2729586
carlosglmt <carlosgl.moralestz@gmail.com>
2019-06-06 19:55:08 -0500

    Agrego procedimiento para hacer carga inicial, hasta ahora funciona con la tabla marca

--- sql/s-09-carga-inicial.sql
+++ sql/s-09-carga-inicial.sql
@@ -2,55 +2,103 @@
 --@Fecha creación: 03/06/2019
 --@Descripción: Carga inicial.
 
+connect sys/system as sysdba
 
---Usuario
-insert into usuario(usuario_id,username,nombre,apellido_paterno,
-	email,clave_acceso,es_administrador,es_conductor,es_cliente)
-values(usuario_seq.nextval,'sindro86','Miguel','Pérez',
-	'sindro86@email.com','sindrome',0,1,0);
+create or replace directory DIR_TEMP AS '/tmp/bases'; 
+grant read,write on directory DIR_TEMP to lm_proy_admin;
 
-insert into conductor(usuario_id,num_licencia,num_cedula,foto,descripcion)
-values (usuario_seq.currval,'LP123456','123456789101112131',hextoraw('453d7a34'),
-	'Es como un Toreto pero revolucionado, si quiere llegar 
-	rápido es la mejor opción');
+connect lm_proy_admin/admin
+set serveroutput on
+create or replace procedure ps_insertar_marcas(
+	p_nombre_archivo in varchar2, p_nombre_tabla in varchar2
+) is
+v_archivo UTL_FILE.FILE_TYPE;
+v_linea varchar2(200);
+v_contador number := 0;
+v_posicion number := 0;
 
-insert into tarjeta(tarjeta_id,num_tarjeta,anio_exp,mes_exp,usuario_id)
-values (tarjeta_seq.nextval,1987654321234567,23,11,usuario_seq.currval);
+TYPE t_array IS TABLE OF VARCHAR2(3900)
+   INDEX BY BINARY_INTEGER;
 
-insert into marca(marca_id,nombre,descripcion,categoria)
-values(marca_seq.nextval,'Ford','Go Further',1);
+a_datos t_array;
 
-insert into modelo(modelo_id,nombre,descripcion,marca_id)
-values (modelo_seq.nextval,'Mustang','Buen auto',marca_seq.currval);
+begin 
+	v_archivo := UTL_FILE.fopen('DIR_TEMP',p_nombre_archivo,'R');
+	
+	loop
+    begin
+    	UTL_FILE.GET_LINE(v_archivo,v_linea); 
+    	dbms_output.put_line(v_linea);
+    	v_posicion := instr(v_linea,'#',1,1);
+			v_contador := 0;
+			while (v_posicion != 0) loop
+				v_contador := v_contador + 1;
+				a_datos(v_contador) := substr(v_linea,1,v_posicion-1);
+				v_linea := substr(v_linea,v_posicion+1,length(v_linea));
+				v_posicion := instr(v_linea,'#',1,1);
+				dbms_output.put_line(a_datos(v_contador));
+				
+				if v_posicion = 0 then
+					v_contador := v_contador + 1;
+					a_datos(v_contador) := v_linea;
+					dbms_output.put_line(a_datos(v_contador));
+				end if;
+			end loop;
 
-insert into ubicacion(ubicacion_id,longitud,latitud,disponible)
-values (ubicacion_seq.nextval,18.1231231,18.3213212,1);
+			--INSERT
+			if p_nombre_tabla = 'marca' then
+				insert into marca(marca_id,nombre,descripcion,categoria)
+				values (marca_seq.nextval,a_datos(1),a_datos(2),a_datos(3));
+			--elsif p_nombre_tabla = 'modelo' then
+			
+			--elsif p_nombre_tabla = 'auto' then
 
-insert into auto(auto_id,placa,anio,modelo_id,usuario_id,ubicacion_id)
-values (auto_seq.nextval,'PLACA-04',2019,modelo_seq.currval,usuario_seq.currval,
-	ubicacion_seq.currval);
+			--elsif p_nombre_tabla = 'usuario' then
 
-insert into usuario(usuario_id,username,nombre,apellido_paterno,
-	apellido_materno,email,clave_acceso,es_administrador,es_conductor,
-	es_cliente)
-values(usuario_seq.nextval,'joa123','Joaquín','Ramírez',
-	'García','joa123@email.com','clave123',1,0,1);
+			--elsif p_nombre_tabla = 'cliente' then
 
-insert into administrador(usuario_id,codigo,certificado)
-values(usuario_seq.currval,123456,hextoraw('453d7a34'));
+			--elsif p_nombre_tabla = 'administrador' then
 
-insert into cliente(usuario_id,num_celular)
-values (usuario_seq.currval,5512345678);
+			--elsif p_nombre_tabla = 'conductor' then
 
-insert into tarjeta(tarjeta_id,num_tarjeta,anio_exp,mes_exp,usuario_id)
-values (tarjeta_seq.nextval,9876543210123456,22,12,usuario_seq.currval);
+			--elsif p_nombre_tabla = 'ubicacion' then
 
-insert into status_viaje(status_viaje_id,clave,descripcion)
-values (status_viaje_seq.nextval,'PROGRAMADO','Se ha solicitado un viaje.');
+			--elsif p_nombre_tabla = 'factura' then
 
-insert into viaje (viaje_id,latitud_origen,latitud_destino,longitud_origen,
-	longitud_destino,importe,usuario_id,auto_id,
-	status_viaje_id)
-values (viaje_seq.nextval,19.1234567,29.1234567,13.1234567,14.1234567,
-	134,usuario_seq.currval,auto_seq.currval,status_viaje_seq.currval);
+			--elsif p_nombre_tabla = 'status_viaje' then
 
+			--elsif p_nombre_tabla = 'viaje' then
+
+			--elsif p_nombre_tabla = 'historico_status_viaje' then
+
+			--elsif p_nombre_tabla = 'tarjeta' then
+
+			--elsif p_nombre_tabla = 'tarjeta_viaje' then
+
+			--elsif p_nombre_tabla = 'pago' then
+
+			end if;
+
+    	exception
+    		when No_Data_Found then  
+    			exit; 
+    end;
+  end loop;
+
+end;
+/
+show errors
+
+
+Prompt Creando directorio /tmp/bases
+!rm -rf /tmp/bases 
+!mkdir -p /tmp/bases 
+Prompt Cambiando permisos
+!chmod 777 /tmp/bases 
+
+!chmod 777 marcas.txt 
+
+Prompt Copiando archivo csv a /tmp/bases 
+!cp marcas.txt /tmp/bases 
+
+exec ps_insertar_marcas('marcas.txt','marca');
